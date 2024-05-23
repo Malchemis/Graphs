97205b1ed17e6fed66ce89d044f91e9e08a4cf3b
malchemis <malchemis@proton.me>
2024-05-23 20:02:18 +0200

    rapport

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from tqdm import tqdm
 import time
 
 
-def main(n_iter=100, problem=Problems.SHORTEST_PATH, algo=Strategies.A_STAR, file_path="examples/reseau_50_50_1.txt", display=True, verbose=False,
+def main(n_iter=100, problem=Problems.SHORTEST_PATH, algo=Strategies.A_STAR, file_path="examples/reseau_50_50_1.txt",
+         display=True, verbose=False,
          save=False):
     """Create a graph from the given file and display it."""
     graph = Graph(file_path, problem)
@@ -42,5 +43,5 @@ def compare_shortest_path_algo(n_iter=100, problem=Problems.SHORTEST_PATH, file_
 if __name__ == '__main__':
     gen_tsp(30, .2)
     main(verbose=True, problem=Problems.TSP, algo=Strategies.BRUTE_FORCE, n_iter=1, display=True, save=True,
-    file_path="examples/tsp.txt")
+         file_path="examples/tsp.txt")
     # compare_shortest_path_algo(n_iter=100, file_path="examples/tsp1.txt", problem=Problems.TSP)
